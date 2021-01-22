fac0937619c9de904477fb367dcd5812fa16e850
yanuaraditia <yanuaraditia@outlook.com>
2021-01-22 22:50:59 +0700

    S

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,8 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: ''}
+        content: ''
+      }
     ],
     link: [
       {
@@ -41,19 +42,17 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   bootstrapVue: {
     icons: false,
-    bootstrapCSS: false, 
+    bootstrapCSS: false,
     bootstrapVueCSS: false
   },
 
@@ -71,6 +70,5 @@ export default {
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+  build: {}
 }
